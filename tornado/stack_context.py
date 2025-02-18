7042a7c41782547e4a5760f15ea5f407060d5dc5
Eugene Molotov <molotov@it-projects.info>
2025-02-18 16:36:37 +0500

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add support for python 3.10+

--- tornado/stack_context.py
+++ tornado/stack_context.py
@@ -191,9 +191,7 @@ class ExceptionStackContext(object):
     def __init__(self, exception_handler, delay_warning=False):
         self.delay_warning = delay_warning
         if not self.delay_warning:
-            warnings.warn(
-                "StackContext is deprecated and will be removed in Tornado 6.0",
-                DeprecationWarning)
+            pass
         self.exception_handler = exception_handler
         self.active = True
 
@@ -203,9 +201,7 @@ class ExceptionStackContext(object):
     def exit(self, type, value, traceback):
         if type is not None:
             if self.delay_warning:
-                warnings.warn(
-                    "StackContext is deprecated and will be removed in Tornado 6.0",
-                    DeprecationWarning)
+                pass
             return self.exception_handler(type, value, traceback)
 
     def __enter__(self):
